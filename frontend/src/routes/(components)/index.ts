962db1f1df29a673277855fd1c304880612da403
Sam Lewis <sam.vr.lewis@gmail.com>
2024-04-27 10:41:45 +1000

    Start of frontend

---
+++ frontend/src/routes/(components)/index.ts
@@ -0,0 +1,1 @@
+export { default as CardsAlertSettings } from "./alert-settings.svelte";
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import { CardsAlertSettings } from './(components)/index.js';
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<CardsAlertSettings />
