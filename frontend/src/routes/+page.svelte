37571b160fb6ae145695bd6e7e2ca6e2261c7a15
Sam Lewis <sam.vr.lewis@gmail.com>
2024-05-29 15:00:14 +1000

    fe: Improve layout
    Makes uniform spacing between cards and makes 1 column in medium and
    small sizes, for better viewing.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,20 +3,20 @@
 	import { Button } from '$lib/components/ui/button';
 </script>
 
-<div class="container mx-auto md:max-w-7xl">
+<div class="container mx-auto px-4 sm:px-6 md:max-w-7xl md:px-8">
 	<div
-		class="items-start justify-center gap-5 rounded-lg p-8 md:grid lg:grid-cols-2 xl:grid-cols-2"
+		class="items-start justify-center gap-5 rounded-lg p-4 sm:p-6 md:grid md:grid-cols-1 md:p-8 lg:grid-cols-2 xl:grid-cols-2"
 	>
-		<div class="col-span-2 grid items-start gap-5 lg:col-span-2">
-			<h1 class="text-xl font-medium">
+		<div class="col-span-1 grid items-start gap-5 lg:col-span-2">
+			<h1 class="mb-5 text-xl font-medium md:mb-0">
 				<img src="whistle.png" class="inline" alt="icon" />&nbsp; Footy Alerts
 			</h1>
 		</div>
-		<div class="col-span-2 grid items-start gap-5 md:col-span-1 lg:col-span-1">
+		<div class="col-span-1 grid items-start gap-5 lg:col-span-1">
 			<CardsAlertSettings />
 			<CardsAbout />
 		</div>
-		<div class="col-span-2 grid items-start gap-5 md:col-span-1 lg:col-span-1">
+		<div class="col-span-1 grid items-start gap-5 lg:col-span-1">
 			<CardsScores />
 		</div>
 	</div>
