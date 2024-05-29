9e94ae7944f01af23d8f3b972c853223fd47312b
Sam Lewis <sam.vr.lewis@gmail.com>
2024-05-29 14:58:15 +1000

    fe: Connect to real games endpoint

--- frontend/src/routes/(components)/scores.svelte
+++ frontend/src/routes/(components)/scores.svelte
@@ -1,140 +1,36 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Card from '$lib/components/ui/card/index.js';
-	import { Label } from '$lib/components/ui/label/index.js';
-	import { Switch } from '$lib/components/ui/switch/index.js';
+	import { PUBLIC_API_BASE_URL } from '$env/static/public';
 
-	const games = [
-		{
-			id: 35754,
-			round: 7,
-			home_team: 'Richmond',
-			away_team: 'Melbourne',
-			complete: 100,
-			winner: null,
-			home_score: 42,
-			away_score: 85,
-			timestr: 'Full Time',
-			year: 2024,
-			date: '2024-04-24 19:25:00',
-			tz: '+10:00'
-		},
-		{
-			id: 35755,
-			round: 7,
-			home_team: 'Essendon',
-			away_team: 'Collingwood',
-			complete: 99,
-			winner: null,
-			home_score: 85,
-			away_score: 85,
-			timestr: 'Full Time',
-			year: 2024,
-			date: '2024-04-25 15:20:00',
-			tz: '+10:00'
-		},
-		{
-			id: 35756,
-			round: 7,
-			home_team: 'GWS',
-			away_team: 'Brisbane',
-			complete: 100,
-			winner: null,
-			home_score: 113,
-			away_score: 59,
-			timestr: 'Full Time',
-			year: 2024,
-			date: '2024-04-25 19:30:00',
-			tz: '+10:00'
-		},
-		{
-			id: 35757,
-			round: 7,
-			home_team: 'Port Adelaide',
-			away_team: 'St Kilda',
-			complete: 100,
-			winner: null,
-			home_score: 82,
-			away_score: 72,
-			timestr: 'Full Time',
-			year: 2024,
-			date: '2024-04-26 19:40:00',
-			tz: '+10:00'
-		},
-		{
-			id: 35758,
-			round: 7,
-			home_team: 'North Melbourne',
-			away_team: 'Adelaide',
-			complete: 24,
-			winner: null,
-			home_score: 14,
-			away_score: 35,
-			timestr: 'Q1 29:12',
-			year: 2024,
-			date: '2024-04-27 13:45:00',
-			tz: '+10:00'
-		},
-		{
-			id: 35759,
-			round: 7,
-			home_team: 'Geelong',
-			away_team: 'Carlton',
-			complete: 0,
-			winner: null,
-			home_score: 0,
-			away_score: 0,
-			timestr: 'Not started',
-			year: 2024,
-			date: '2024-04-27 16:35:00',
-			tz: '+10:00'
-		},
-		{
-			id: 35760,
-			round: 7,
-			home_team: 'Fremantle',
-			away_team: 'Western Bulldogs',
-			complete: 0,
-			winner: null,
-			home_score: 0,
-			away_score: 0,
-			timestr: 'Not started',
-			year: 2024,
-			date: '2024-04-27 19:30:00',
-			tz: '+10:00'
-		},
-		{
-			id: 35761,
-			round: 7,
-			home_team: 'Gold Coast',
-			away_team: 'West Coast',
-			complete: 0,
-			winner: null,
-			home_score: 0,
-			away_score: 0,
-			timestr: 'Not started',
-			year: 2024,
-			date: '2024-04-28 13:00:00',
-			tz: '+10:00'
-		},
-		{
-			id: 35762,
-			round: 7,
-			home_team: 'Hawthorn',
-			away_team: 'Sydney',
-			complete: 0,
-			winner: null,
-			home_score: 0,
-			away_score: 0,
-			timestr: 'Not started',
-			year: 2024,
-			date: '2024-04-28 16:00:00',
-			tz: '+10:00'
+	import { onMount } from 'svelte';
+	import { writable, derived } from 'svelte/store';
+
+	export const gamesApiData = writable<any[]>([]);
+
+	export const games = derived(gamesApiData, ($gamesApiData) => {
+		if (Array.isArray($gamesApiData)) {
+			return $gamesApiData;
 		}
-	];
+		return [];
+	});
+
+	onMount(async () => {
+		try {
+			const response = await fetch(`${PUBLIC_API_BASE_URL}/games`);
+			const data = await response.json();
+			if (Array.isArray(data)) {
+				gamesApiData.set(data);
+			} else {
+				console.error('Fetched data is not an array:', data);
+				gamesApiData.set([]);
+			}
+		} catch (error) {
+			console.error('Error fetching games data:', error);
+			gamesApiData.set([]);
+		}
+	});
 
 	function converToIconName(input: string): string {
-		// Convert the input string to lowercase and remove spaces using regex
 		return input.toLowerCase().replace(/\s/g, '') + '.png';
 	}
 
@@ -150,31 +46,19 @@
 
 		const datetime = new Date(date + ' ' + tz);
 
-		// Check if the parsed date is valid
 		if (isNaN(datetime.getTime())) {
-			// If parsing fails, return the original string
 			return date;
 		}
 
-		// Create an array to map the days of the week
 		const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-		// Get the day of the week from the Date object
 		const dayOfWeek = daysOfWeek[datetime.getDay()];
-
-		// Get the hour and minute from the Date object
 		let hour = datetime.getHours();
 		const minute = datetime.getMinutes();
-
-		// Convert hour to 12-hour format
 		const ampm = hour >= 12 ? 'PM' : 'AM';
 		hour = hour % 12;
-		hour = hour ? hour : 12; // Handle midnight
+		hour = hour ? hour : 12;
 
-		// Format the time string
 		const timeString = `${hour}.${minute < 10 ? '0' : ''}${minute}${ampm}`;
-
-		// Combine day of the week and time
 		return `${dayOfWeek} ${timeString}`;
 	}
 </script>
@@ -187,7 +71,7 @@
 	<Card.Content>
 		<div>
 			<div class="grid grid-cols-1 items-center gap-5">
-				{#each games as game}
+				{#each $games as game}
 					<div class="grid grid-cols-4 items-center gap-1 lg:grid-cols-3">
 						<div class="col-span-2 lg:col-span-1">
 							<p class="text-sm font-medium leading-none">
