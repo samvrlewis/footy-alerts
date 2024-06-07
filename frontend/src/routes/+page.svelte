e4016d6ce2da874baaa4b659d00d5ab731284420
Sam Lewis <sam.vr.lewis@gmail.com>
2024-06-07 15:52:29 +1000

    fe: Fix margins in different sizes

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,21 +6,23 @@
 
 <div class="container mx-auto px-4 sm:px-6 md:max-w-7xl md:px-8">
 	<div
-		class="items-start justify-center gap-5 rounded-lg p-4 sm:p-6 md:grid md:grid-cols-1 md:p-8 lg:grid-cols-2 xl:grid-cols-2"
+		class="items-start justify-center gap-x-5 rounded-lg p-4 sm:p-6 md:grid md:grid-cols-1 md:p-8 lg:grid-cols-2 xl:grid-cols-2"
 	>
-		<div class="col-span-1 grid items-start gap-5 lg:col-span-2">
+		<div class="col-span-1 mb-4 grid items-start gap-5 lg:col-span-2">
 			<h1 class="mb-5 text-xl font-medium md:mb-0">
 				<img src="favicon-32x32.png" class="inline" alt="icon" />&nbsp; Footy Alerts
 			</h1>
 		</div>
-		<div class="grid items-start gap-5 lg:col-span-1">
-			<CardsAlertSettings />
+		<div class="grid items-start lg:col-span-1">
+			<div class="mb-4 sm:mb-4">
+				<CardsAlertSettings />
+			</div>
 			<div class="hidden lg:block">
 				<CardsAbout />
 			</div>
 		</div>
-		<div class="grid items-start gap-5 lg:col-span-1">
-			<CardsScores />
+		<div class="grid items-start lg:col-span-1">
+			<div class="mb-4 sm:mb-4"><CardsScores /></div>
 			<div class="lg:hidden">
 				<CardsAbout />
 			</div>
